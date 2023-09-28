9310ec0dc23484dc5827a492a9722cde409d8303
Shadevliya02 <devrinasharla@gmail.com>
2023-09-28 14:46:27 +0700

    firstt

--- Jurnal01.java
+++ Jurnal01.java
@@ -1,8 +1,8 @@
 public class Jurnal01{
     public static void main (String[] args) {
-        System.out.println("Nama : Sharla Devrina Aurelliya");
-        System.out.println("NIM : 607062330139");
-        System.out.println("Kelas : D3IF-47-01");
+        System.out.println("Nama" ":" "Sharla Devrina Aurelliya");
+        System.out.println("NIM" ":" "607062330139");
+        System.out.println("Kelas" ":" "D3IF-47-01");
     }
 
 }
